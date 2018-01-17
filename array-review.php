07e099eba2d376242b81ce210c07d52be4d2bd83
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-01-16 23:25:26 -0500

    added sortFunction and addFunction

--- array-review.php
+++ array-review.php
@@ -6,4 +6,24 @@
  * Time: 10:23 AM
  */
 
-$animals = array("panada", "alpaca", "boa");
+$animals = array("panda", "alpaca", "boa");
+
+function sortFunction($array) {
+    sort($array);
+    foreach ($array as $element) {
+        echo "$element ";
+    }
+}
+
+function addFunction($string, $animals){
+    echo "<p>adding $string ...</p> ";
+    $string = strtolower($string);
+    if(!(in_array($string, $animals))) {
+        array_push($animals, $string);
+    }
+    sortFunction($animals);
+}
+sortFunction($animals);
+
+addFunction("goat", $animals);
+
